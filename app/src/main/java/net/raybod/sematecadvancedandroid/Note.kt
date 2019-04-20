ab07c522ec8755b796ac40a384c936127e8f9480
e.safarzadeh <e.safarzadeh@RYB-W10-08>
2019-04-20 16:21:31 +0430

    Initial commit

---
+++ app/src/main/java/net/raybod/sematecadvancedandroid/Note.kt
@@ -0,0 +1,7 @@
+package net.raybod.sematecadvancedandroid
+
+class Note(nodeID: Int, nodeName: String, nodeDes: String) {
+    var nodeID: Int? = nodeID
+    var nodeName: String? = nodeName
+    var nodeDes: String? = nodeDes
+}
